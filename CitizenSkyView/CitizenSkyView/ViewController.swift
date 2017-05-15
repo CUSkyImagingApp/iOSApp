f102d805d108c5d7428ed84016b01c9d721517f6
jrapp <jeremy.rapp@colorado.edu>
2017-05-15 12:49:54 -0600

    Adding helper functions for image capture

--- CitizenSkyView/CitizenSkyView/ViewController.swift
+++ CitizenSkyView/CitizenSkyView/ViewController.swift
@@ -9,13 +9,16 @@
 import UIKit
 import AWSCore
 import AWSCognito
+import AWSS3
+import AVFoundation
 
 class ViewController: UIViewController {
     
     
     //MARK: Properties
-    
-    
+    let captureSession = AVCaptureSession()
+    var capturePhotoOutput = AVCapturePhotoOutput()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +37,107 @@ class ViewController: UIViewController {
                                                                 identityPoolId:"us-west-2:43473766-619f-4209-996b-7dc61e65ccf1")
         let configuration = AWSServiceConfiguration(region:.USWest2, credentialsProvider:credentialsProvider)
         AWSServiceManager.default().defaultServiceConfiguration = configuration
+        
+        
+        //Check to see if we have camera permission
+        self.checkCameraAuthorization { authorized in
+            if authorized {
+                // Proceed to set up and use the camera.
+                
+                
+            } else {
+                print("Permission to use camera denied.")
+                return
+            }
+        }
+        
+        let transferManager = AWSS3TransferManager.default()
+        let uploadRequest = AWSS3TransferManagerUploadRequest()
+        uploadRequest?.bucket = "cu-sky-imager"
+        uploadRequest?.key = "test-ios-image"
+        //uploadRequest.body = uploadingfileurl
+        //uploadRequest.contentLength = fileSize
+        
+        transferManager.upload(uploadRequest!).continueWith(executor: AWSExecutor.mainThread(), block: { (task:AWSTask<AnyObject>) -> Any? in
+            print("Uploaded")
+        })
+        
+    }
+    
+    
+    
+    func checkCameraAuthorization(_ completionHandler: @escaping ((_ authorized: Bool) -> Void)) {
+        switch AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo) {
+        case .authorized:
+            //The user has previously granted access to the camera.
+            completionHandler(true)
+            
+        case .notDetermined:
+            // The user has not yet been presented with the option to grant video access so request access.
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { success in
+                completionHandler(success)
+            })
+            
+        case .denied:
+            // The user has previously denied access.
+            completionHandler(false)
+            
+        case .restricted:
+            // The user doesn't have the authority to request access e.g. parental restriction.
+            completionHandler(false)
+        }
+    }
+    
+    
+    
+    
+    func defaultDevice() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.defaultDevice(withDeviceType: .builtInDualCamera,
+                                                      mediaType: AVMediaTypeVideo,
+                                                      position: .back) {
+            return device // use dual camera on supported devices
+        } else if let device = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera,
+                                                             mediaType: AVMediaTypeVideo,
+                                                             position: .back) {
+            return device // use default back facing camera otherwise
+        } else {
+            fatalError("All supported devices are expected to have at least one of the queried capture devices.")
+        }
+    }
+    
+    
+    
+    
+    func configureCaptureSession(_ completionHandler: ((_ success: Bool) -> Void)) {
+        var success = false
+        defer { completionHandler(success) } // Ensure all exit paths call completion handler.
+        
+        // Get video input for the default camera.
+        let videoCaptureDevice = defaultDevice()
+        guard let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice) else {
+            print("Unable to obtain video input for default camera.")
+            return
+        }
+        
+        // Create and configure the photo output.
+        let capturePhotoOutput = AVCapturePhotoOutput()
+        capturePhotoOutput.isHighResolutionCaptureEnabled = true
+        capturePhotoOutput.isLivePhotoCaptureEnabled = capturePhotoOutput.isLivePhotoCaptureSupported
+        
+        // Make sure inputs and output can be added to session.
+        guard self.captureSession.canAddInput(videoInput) else { return }
+        guard self.captureSession.canAddOutput(capturePhotoOutput) else { return }
+        
+        // Configure the session.
+        self.captureSession.beginConfiguration()
+        self.captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        self.captureSession.addInput(videoInput)
+        self.captureSession.addOutput(capturePhotoOutput)
+        self.captureSession.commitConfiguration()
+        
+        self.capturePhotoOutput = capturePhotoOutput
+        
+        success = true
     }
 
 }
